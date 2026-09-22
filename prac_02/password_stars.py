559349497f0945a77b11ccdba92a6e019ab04957
jd162312 <hayden.sadler@my.jcu.edu.au>
2026-09-22 12:39:25 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,23 @@
+"""Convert password into *'s"""
+
 MIN_LENGTH = 5
-password = input("Enter password: ")
-while len(password) < MIN_LENGTH:
-    print("Invalid password")
+
+
+def main():
+    password = get_password()
+    print_password(password)
+
+
+def print_password(password: str):
+    print("*" * len(password))
+
+
+def get_password() -> str:
     password = input("Enter password: ")
-print("*" * len(password))
+    while len(password) < MIN_LENGTH:
+        print("Invalid password")
+        password = input("Enter password: ")
+    return password
+
+
+main()
